eb56285382b54083c169bb735fb8cbb05e4ee102
Rishav Naskar <rishavnaskar.r@gmail.com>
2021-08-02 13:42:54 +0530

    feat: implemented Spikes vidoe sections and started posts section in home page

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -2,23 +2,14 @@
   <v-app>
     <v-app-bar app color="#1B1E27" dark>
       <v-spacer></v-spacer>
-      <v-btn
-        class="profile-btn"
-        depressed
-        elevation="2"
-        icon
-        raised
-        rounded
-        small
-        min-width="50"
-      >
+      <v-btn depressed elevation="2" icon raised rounded small min-width="50">
         <v-avatar size="30" color="#57BAFF">
           <v-icon color="#1B1E27">mdi-account-outline</v-icon>
         </v-avatar>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main style="background-color: #252a36">
       <router-view />
     </v-main>
   </v-app>
--- FrontEnd/src/components/HelloWorld.vue
+++ FrontEnd/src/components/HelloWorld.vue
@@ -1,74 +1,80 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
+    <v-row>
+      <h1 class="spikeText">Spikes</h1>
+      <v-spacer></v-spacer>
+      <v-btn
+        class="spikeBtn popular white--text"
+        depressed
+        elevation="2"
+        raised
+        rounded
+        small
+        color="#9673ff"
+        >Popular
+      </v-btn>
+      <v-btn
+        class="spikeBtn recent white--text"
+        depressed
+        elevation="2"
+        raised
+        rounded
+        small
+        color="#57BAff"
+        >Recent
+      </v-btn>
+    </v-row>
+    <template>
+      <v-item-group class="spikeVideos">
+        <v-container>
+          <v-row>
+            <v-col v-for="n in 6" :key="n" cols="12" md="2">
+              <v-item v-slot="{ active, toggle }">
+                <v-card
+                  :color="active ? 'primary' : ''"
+                  class="d-flex align-center"
+                  dark
+                  height="250"
+                  width="200"
+                  elevation="6"
+                  @click="toggle"
+                >
+                  <v-scroll-y-transition>
+                    <div v-if="active" class="text-h2 flex-grow-1 text-center">
+                      Active
+                    </div>
+                  </v-scroll-y-transition>
+                </v-card>
+              </v-item>
+              <p class="spikeVideoSubtitle">17 chained 🔥</p>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-item-group>
+    </template>
+    <v-row>
+      <h1 class="postsText">Posts</h1>
+      <v-spacer></v-spacer>
+      <v-btn
+        class="postsBtn popular white--text"
+        depressed
+        elevation="2"
+        raised
+        rounded
+        small
+        color="#9673ff"
+        >Popular
+      </v-btn>
+      <v-btn
+        class="postsBtn recent white--text"
+        depressed
+        elevation="2"
+        raised
+        rounded
+        small
+        color="#57BAff"
+        >Recent
+      </v-btn>
     </v-row>
   </v-container>
 </template>
@@ -76,58 +82,60 @@
 <script>
 export default {
   name: "HelloWorld",
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
-  }),
+  data: () => ({}),
 };
 </script>
+
+<style>
+.spikeText {
+  color: #57baff;
+  margin-top: 1%;
+  margin-left: 2%;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.spikeVideos {
+  margin-top: 2%;
+}
+
+.spikeVideoSubtitle {
+  margin-top: 10px;
+  margin-left: 1%;
+  color: #979797;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.spikeBtn {
+  margin-top: 2%;
+  text-transform: none;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.popular {
+  opacity: 1;
+  margin-right: 1%;
+}
+
+.recent {
+  opacity: 0.7;
+  margin-right: 2%;
+}
+
+.postsText {
+  color: #57baff;
+  margin-top: 3%;
+  margin-left: 2%;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.postsBtn {
+  margin-top: 4%;
+  text-transform: none;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
