d8c81437ab8836adc14446022046f08bbcb64d4d
Brandon Chung <deltaharbinger@gmail.com>
2021-08-05 20:48:05 -0500

    Added state management

--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import posts from "./modules/posts";
+import auth from "./modules/auth";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        posts,
+        auth,
+    },
 });
